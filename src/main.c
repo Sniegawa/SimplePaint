4e62c3e6e9c943d8a259a5835fa367e4281c1e4d
Mikołaj Lisowski <lisekmiki@gmail.com>
2025-11-29 18:05:22 +0100

    Nuklear demo implemented

--- src/main.c
+++ src/main.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
-#define NK_IMPLEMENTATION
-#include <nuklear.h>
-
 #include <glad/glad.h>
-
-//#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+
+#define NK_IMPLEMENTATION
+#define NK_GLFW_GL4_IMPLEMENTATION
+#include <nuklear.h>
+#include <demo/glfw_opengl4/nuklear_glfw_gl4.h>
+
+
+#define MAX_VERTEX_BUFFER 512 * 1024
+#define MAX_ELEMENT_BUFFER 128 * 1024
+
 
 //GLFW callbacks
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -17,16 +31,41 @@ int main(int argc, char** argv)
 {
 	GLFWwindow* window = InitLibraries();
 
+	//init nuklear
+	struct nk_context* ctx = nk_glfw3_init(window, NK_GLFW3_INSTALL_CALLBACKS, MAX_VERTEX_BUFFER, MAX_ELEMENT_BUFFER);
+
+	//load fonts
+	struct nk_font_atlas* atlas;
+	nk_glfw3_font_stash_begin(&atlas);
+	// For loading fonts Nuklear/demo/glfw_opengl4/main.c 127
+	nk_glfw3_font_stash_end();
+
 
 	while(!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
 
+		nk_glfw3_new_frame();
 
+		if(nk_begin(ctx,"Demo Window",nk_rect(50,50,230,150),
+			NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE | NK_WINDOW_CLOSABLE | NK_WINDOW_TITLE))
+		{
+
+			nk_layout_row_dynamic(ctx, 30, 1);
+			nk_label(ctx, "Hello from Nuklear!", NK_TEXT_LEFT);
+
+			if (nk_button_label(ctx, "Press me!"))
+				printf("Button pressed!\n");
+		}
+		nk_end(ctx);
 
 
 		glClearColor(0.2, 0.2, 0.2, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		nk_glfw3_render(NK_ANTI_ALIASING_ON);
+
 		glfwSwapBuffers(window);
 	}
 
@@ -34,6 +73,7 @@ int main(int argc, char** argv)
 	glfwTerminate();
 	return 0;
 }
+
 GLFWwindow* InitLibraries()
 {
 	if (glfwInit() == GLFW_FALSE)
@@ -69,6 +109,7 @@ GLFWwindow* InitLibraries()
 
 	return window;
 }
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
